45af2937af959aa5e3377d51971b91da44527722
Keturah Howard and Mariam Alaeddin <mariam@>
2020-02-04 10:12:55 -0800

    clean up lint within main and script files

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
-import { Triangle } from './scripts.js'
+import { Triangle } from './scripts.js';
 
 function chooseDisplay(triangle) {
-    if (triangle.equaleteral) {
-        $(".equilateral").show();
-    } else if (triangle.isosceles) {
-        $(".isosceles").show();
-    } else if (triangle.scalene) {
-        $(".scalene").show();
-    } else {
-        $(".not-triangle").show();
-    }
+  if (triangle.equaleteral) {
+    $(".equilateral").show();
+  } else if (triangle.isosceles) {
+    $(".isosceles").show();
+  } else if (triangle.scalene) {
+    $(".scalene").show();
+  } else {
+    $(".not-triangle").show();
+  }
 }
 
 $(document).ready(function() {
-    $("form#quiz").submit(function(event) {
-      event.preventDefault();
-      var side1 = parseInt($("#side1").val());
-      var side2 = parseInt($("#side2").val());
-      var side3 = parseInt($("#side3").val());
-      var triangle = new Triangle(side1, side2, side3);
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
+    var triangle = new Triangle(side1, side2, side3);
 
-      triangle.decideType()
-      chooseDisplay(triangle)
-    });
+    triangle.decideType();
+    chooseDisplay(triangle);
   });
+});
--- src/scripts.js
+++ src/scripts.js
@@ -1,39 +1,41 @@
 
-  export function Triangle(side1, side2, side3) {
-    this.equalateral = false;
-    this.isosceles = false;
-    this.scalene = false;
-    this.isNotTriangle = true;
-    
-  }
-  
-  
-  Triangle.prototype.decideType = function () {
-    if (side1 + side2 > side3) {
-      if (side1 === side2 === side3) {
-        this.equalateral = true;
-      } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-        this.isosceles = true
-      } else {
-        this.scalene = true
-      }
-    } else if (side2 + side3 > side1) {
-      if (side1 === side2 === side3) {
-        this.equalateral = true;
-      } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-        this.isosceles = true
-      } else {
-        this.scalene = true
-      }
-    } else if (side3 + side1 > side2) {
-      if (side1 === side2 === side3) {
-        this.equalateral = true;
-      } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-        this.isosceles = true
-      } else {
-        this.scalene = true
-      }
+export function Triangle(side1, side2, side3) {
+  this.side1 = side1;
+  this.side2 =  side2;
+  this.side3 = side3;
+  this.equalateral = false;
+  this.isosceles = false;
+  this.scalene = false;
+  this.isNotTriangle = true;
+}
+
+
+Triangle.prototype.decideType = function () {
+  if (this.side1 +  this.side2 > this.side3) {
+    if (this.side1 ===  this.side2 === this.side3) {
+      this.equalateral = true;
+    } else if (this.side1 ===  this.side2 ||  this.side2 === this.side3 || this.side3 === this.side1) {
+      this.isosceles = true;
     } else {
-      this.isNotTriangle = false;
+      this.scalene = true;
     }
+  } else if ( this.side2 + this.side3 > this.side1) {
+    if (this.side1 ===  this.side2 === this.side3) {
+      this.equalateral = true;
+    } else if (this.side1 ===  this.side2 ||  this.side2 === this.side3 || this.side3 === this.side1) {
+      this.isosceles = true;
+    } else {
+      this.scalene = true;
+    }
+  } else if (this.side3 + this.side1 >  this.side2) {
+    if (this.side1 ===  this.side2 === this.side3) {
+      this.equalateral = true;
+    } else if (this.side1 ===  this.side2 ||  this.side2 === this.side3 || this.side3 === this.side1) {
+      this.isosceles = true;
+    } else {
+      this.scalene = true;
+    }
+  } else {
+    this.isNotTriangle = false;
   }
+};
