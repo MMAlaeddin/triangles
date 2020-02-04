2fd1f09251c224de014ef45e4605f850ee595491
Keturah Howard and Mariam Alaeddin <keturah@>
2020-02-04 09:51:59 -0800

    refactor our javascript into seperate back/front ends, and import into main js. create function for displaying

--- src/scripts.js
+++ src/scripts.js
@@ -1,36 +1,39 @@
-$(document).ready(function() {
-  $("form#quiz").submit(function(event) {
-    event.preventDefault();
-    var side1 = parseInt($("#side1").val());
-    var side2 = parseInt($("#side2").val());
-    var side3 = parseInt($("#side3").val());
 
+  export function Triangle(side1, side2, side3) {
+    this.equalateral = false;
+    this.isosceles = false;
+    this.scalene = false;
+    this.isNotTriangle = true;
+    
+  }
+  
+  
+  Triangle.prototype.decideType = function () {
     if (side1 + side2 > side3) {
       if (side1 === side2 === side3) {
-        $(".equilateral").show();
+        this.equalateral = true;
       } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-        $(".isosceles").show();
+        this.isosceles = true
       } else {
-        $(".scalene").show();
+        this.scalene = true
       }
     } else if (side2 + side3 > side1) {
       if (side1 === side2 === side3) {
-        $(".equilateral").show();
+        this.equalateral = true;
       } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-        $(".isosceles").show();
+        this.isosceles = true
       } else {
-        $(".scalene").show();
+        this.scalene = true
       }
-    } else if ( side3 + side1 > side2) {
+    } else if (side3 + side1 > side2) {
       if (side1 === side2 === side3) {
-        $(".equilateral").show();
+        this.equalateral = true;
       } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-        $(".isosceles").show();
+        this.isosceles = true
       } else {
-        $(".scalene").show();
+        this.scalene = true
       }
     } else {
-      $('.not-triangle')
+      this.isNotTriangle = false;
     }
-  });
-});
+  }
